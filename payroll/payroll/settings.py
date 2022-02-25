eca82fb69995700d87590f78a72fb497e71bc23e
abhinayy0 <abhinayy0@gmail.com>
2022-02-25 10:13:33 +0530

    Updated settings to add value from env

--- payroll/payroll/settings.py
+++ payroll/payroll/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_j=er01w+n&)65h*8nmrb8p8cx@s6d&&li__1jewddqt*4^%zt'
+SECRET_KEY = os.environ.get("SECRET_KEY",'django-insecure-_j=er01w+n&)65h*8nmrb8p8cx@s6d&&li__1jewddqt*4^%zt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
